1177c481c8a6a86084ba4a67adabecfff7c7f7cc
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-07-13 17:14:05 -0400

    Minimal example translator

---
+++ example/translator.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+from mediachain.translation.translator import Translator
+
+
+class ExampleTranslator(Translator):
+
+    @staticmethod
+    def translator_id():
+        return 'ExampleTranslator'
+
+    @staticmethod
+    def translate(parsed_metadata):
+        example_json = parsed_metadata
+
+        data = {
+            '_id': example_json['id']
+        }
+
+        artwork_artefact = {
+            u'__mediachain_object__': True,
+            u'type': u'artefact',
+            u'meta': {'data': data}
+        }
+
+        return {
+            u'canonical': artwork_artefact,
+            u'chain': []
+        }
+
+    @staticmethod
+    def can_translate_file(file_path):
+        return True
+
