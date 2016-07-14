6dd93066a64207f58e58b9d1ce41aed8c197d84d
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-07-14 16:30:24 -0400

    Publish to IPFS

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+import ipfsApi
 import os, sys
-from setuptools import setup, find_packages
+from pprint import pprint
+from requests.exceptions import ConnectionError
+from setuptools import setup, find_packages, Command
 from setuptools.command.build_py import build_py as _build_py
+
 
 reqs_file = os.path.join(os.path.dirname(os.path.realpath(__file__))
                    , "requirements.txt")
@@ -10,16 +14,49 @@ reqs = None
 with open(reqs_file) as f:
     reqs = f.readlines()
 
-#def _pre_build_py(dir):
-#    from subprocess import check_call
-#    check_call(['scripts/build_grpc.sh', sys.executable],
-#            cwd=dir)
+class ChDir(object):
+    """
+    Step into a directory temporarily
+    """
+    def __init__(self, path):
+        self.old_dir = os.getcwd()
+        self.new_dir = path
+ 
+    def __enter__(self):
+        os.chdir(self.new_dir)
+ 
+    def __exit__(self, *args):
+        os.chdir(self.old_dir)
 
-#class build_py(_build_py):
-#    def run(self):
-#        self.execute(_pre_build_py, [os.path.dirname(__file__)],
-#                     msg="Generating protobuf")
-#        _build_py.run(self)
+class PublishTranslators(Command):
+    user_options = []
+
+    def initialize_options(self):
+        """N/A"""
+
+    def finalize_options(self):
+        """N/A"""
+
+    def run(self):
+        print(" => IPFS READY!")
+        host = "localhost"
+        port = 5001
+        self.client = ipfsApi.Client(host, port)
+        try:
+            self.client.id()
+        except ConnectionError:
+            raise Exception(
+                'Unable to connect to ipfs API at {}:{} \n'
+                'For ipfs installation instructions see '
+                'https://ipfs.io/docs/install'.format(host, port)
+            )
+        res = {}
+        with ChDir('translators'):
+            for translator in os.listdir('.'):
+                print "adding " + translator
+                res[translator] = self.client.add(translator)['Hash']
+        pprint(res)
+
 
 setup(
     version='0.1.2',
@@ -28,14 +65,9 @@ setup(
     author='Mediachain Labs',
     packages=find_packages('.'),
     author_email = 'hello@mediachainlabs.com',
-    entry_points={
-        'console_scripts': [
-            #'publish_translators = mediachain.cli.main:main'
-        ]
-    },
     url='http://mediachain.io',
     install_requires=reqs,
-    #cmdclass={'build_py': build_py},
-    setup_requires=['pytest-runner>=2.8'],
+    cmdclass={'publish_translators': PublishTranslators},
+    setup_requires=['pytest-runner>=2.8', 'ipfs-api==0.2.3' ],
     tests_require=['pytest>=2.9.2'],
 )
