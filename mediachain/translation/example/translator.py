512e95bbe494bb6ba562cdfd38048e904219f34f
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-07-20 15:35:47 -0400

    Break up example translator a bit

--- mediachain/translation/example/translator.py
+++ mediachain/translation/example/translator.py
@@ -11,20 +11,24 @@ class Example(Translator):
     @staticmethod
     def translate(example_json):
 
-        data = {
-            '_id': example_json['id']
-        }
-
         artwork_artefact = {
             u'__mediachain_object__': True,
             u'type': u'artefact',
-            u'meta': {'data': data}
+            u'meta': {'data': process_data(example_json)}
         }
 
         return {
             u'canonical': artwork_artefact,
             u'chain': []
         }
+
+    @staticmethod
+    def process_data(example_json):
+        data = {
+            '_id': example_json['id']
+        }
+        return data
+
 
     @staticmethod
     def can_translate_file(file_path):
